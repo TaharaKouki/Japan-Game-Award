a2c85a590f4a36e1a4e6a86bc4da19c83285f65c
田原頌輝 <taharakouki0417@icloud.com>
2017-05-16 17:13:21 +0900

    プロトタイプの途中経過です

---
+++ Assets/script/playermove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playermove : MonoBehaviour {
+
+	private bool isRight = true;
+	private Rigidbody2D rig;
+
+	void Start()
+	{
+		rig = this.GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		//左右キーの入力
+		float h = Input.GetAxis("Horizontal");
+		rig.AddForce(Vector2.right * h * 300f);
+	}
+}
