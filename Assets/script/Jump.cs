a0382a01174642f3727038b69d1b3737644e87e8
田原頌輝 <taharakouki0417@icloud.com>
2017-05-25 13:49:10 +0900

    ジャンプ機能を追加しました。

---
+++ Assets/script/Jump.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+	public float jumpHeight;
+	public Rigidbody2D rb;
+	private bool canJunp = true;
+
+	// Use this for initialization
+//	void Start () 
+//	{
+//		rb = GetComponent<Rigidbody>();
+//	}
+	
+	// Update is called once per frame
+
+//	void Update()
+//	{
+//		float gravity = 1;
+//		float time = 1.5f;
+
+//		rb.AddForce(Vector3.up * Mathf.Sqrt(2*jumpHeight*gravity));
+
+//		if (Input.GetKey("Space")) {
+//			// Spaceが押され続けてる！
+//			rb.AddForce(Vector3.up * Mathf.Sqrt(2*jumpHeight*gravity));
+//		}
+
+		
+//	}
+	void OnCollisionStay2D(Collision2D hit){
+		if (Input.GetKeyDown("space") && canJunp) {
+			// Spaceが押され続けてる！
+			Debug.Log(Vector3.up * jumpHeight);
+			rb.AddForce(Vector3.up * 1000.0f * jumpHeight);
+			canJunp = false;
+		}
+	}
+	void OnCollisionExit2D(Collision2D hit){
+		canJunp = true;
+	}
+}
